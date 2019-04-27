c930d2dc5314b9c3327c34380385e30533381fbc
ozaneki <48448045+ozaneki@users.noreply.github.com>
2019-04-27 18:56:55 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace responsi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            karyawan krywn = new karyawan();
+
+            krywn.nama = "Fauzan";
+            krywn.nik = "2319";
+            krywn.gaji = 1000000;
+
+            krywn.nama2 = "Boo";
+            krywn.nik2 = "2512";
+            krywn.gaji2 = 2000000;
+
+            krywn.infoGaji();
+            Console.WriteLine();
+            krywn.kenaikanGaji();
+            Console.WriteLine();
+            Console.ReadKey();
+
+        }
+    }
+}
